ca9285b4eea3c405bb62bbdd1174f0db47c2660c
Sergei Mishkarudny <sergei.mishkarudny@empired.com>
2022-08-17 11:40:59 +1000

    17/08#2

---
+++ Business/Initialization/ContentDeliveryApiInitialization.cs
@@ -0,0 +1,34 @@
+﻿using EPiServer.ContentApi.Cms;
+using EPiServer.ContentApi.Core.Configuration;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using System;
+using System.Linq;
+
+namespace Bysoft.Optimizely.Business.Initialization
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(ContentApiCmsInitialization))]
+    public class ContentDeliveryApiInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Services.Configure<ContentApiConfiguration>(config =>
+            //config.Default().SetMinimumRoles("contentapiread")
+            config.Default().SetMinimumRoles(String.Empty)
+            .SetSiteDefinitionApiEnabled(false)
+            );
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+            //Add initialization logic, this method is called once after CMS has been initialized
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            //Add uninitialization logic
+        }
+    }
+}
---
+++ Business/Initialization/GeneralEventHandlerInitialization.cs
@@ -0,0 +1,24 @@
+﻿using EPiServer.Core;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using System;
+using System.Linq;
+
+namespace Bysoft.Optimizely.Business.Initialization
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class GeneralEventHandlerInitialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            var contentEventsService = context.Locate.Advanced.GetInstance<IContentEvents>();
+            // register for content events here
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            //Add uninitialization logic
+        }
+    }
+}
